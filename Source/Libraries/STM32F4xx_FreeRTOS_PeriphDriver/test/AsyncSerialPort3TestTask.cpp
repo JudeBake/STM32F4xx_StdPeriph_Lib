621bfe06c04161a492da4c7960b05d85db99d131
JudeBake <julien.bacon.1@gmail.com>
2013-12-07 20:07:41 -0500

    creating AsyncSerialPort3 class and its testing tool. Finishing the renaming of the AsyncSerialPort1 testing tool.

---
+++ Source/Libraries/STM32F4xx_FreeRTOS_PeriphDriver/test/AsyncSerialPort3TestTask.cpp
@@ -0,0 +1,148 @@
+/**
+ * \file AsyncSerialPort3TestTask.cpp
+ *
+ * \date Created on: Dec 7, 2013
+ * \date Last change on: &DATE&
+ *
+ * \author Created by: julien 
+ * \author Last change by: &AUTHOR&
+ *
+ * \version Commit Id: &REVISION&
+ */
+
+#include "AsyncSerialPort3TestTask.h"
+
+typedef enum
+{
+	CHAR_READ_WRITE,
+	STRING_READ_WRITE,
+	STRING_OVERLOAD
+} TestingPhase;
+
+void initTestString(uint8_t* testString, uint32_t stringLength);
+void initOverloadTestStr(uint8_t* ovldTestStr, uint32_t stringLength);
+
+AsyncSerialPort3TestTask::AsyncSerialPort3TestTask()
+{
+	portInstance = AsyncSerialPort3::getInstance();
+	currentStatus = SERIAL_INIT_ERROR;
+	testParity = SERIAL_NO_PARITY;
+	testStopBit = SERIAL_1_STOP_BIT;
+	testDataBit = SERIAL_8_BITS_DATA;
+	testHwFlowCtrl = SERIAL_NO_HW_FLOW_CTRL;
+	testLinkMode = SERIAL_FULL_DUPLEX;
+	testBaudRate = SERIAL_9600_BAUD;
+	testInterSetting = SERIAL_INT_ENABLE;
+
+	character = '\0';
+}
+
+AsyncSerialPort3TestTask::~AsyncSerialPort3TestTask()
+{
+	//close port
+	if (portInstance != NULL)
+	{
+		portInstance->closePort();
+	}
+
+	if (IsValid())
+	{
+		Delete();
+	}
+}
+
+bool AsyncSerialPort3TestTask::HardwareInit()
+{
+	bool opResult = pdFALSE;
+
+	currentStatus = portInstance->portInit(testParity, testStopBit,
+			testDataBit, testHwFlowCtrl, testLinkMode,
+			testBaudRate, SERIAL_PORT3_PREEMP_PRIORITY,
+			SERIAL_PORT3_SUBPRIORITY, testInterSetting);
+
+	if (currentStatus == SERIAL_OK)
+	{
+		opResult = pdTRUE;
+	}
+
+	return opResult;
+}
+
+portBASE_TYPE AsyncSerialPort3TestTask::OnCreate(const portCHAR * const pcName,
+		unsigned portSHORT usStackDepth,
+		unsigned portBASE_TYPE uxPriority)
+{
+	portBASE_TYPE opResult = pdFALSE;
+
+	if (portInstance->getCurrentStatus() == SERIAL_OK)
+	{
+		opResult = pdTRUE;
+	}
+
+	return opResult;
+}
+
+
+void AsyncSerialPort3TestTask::Run(void)
+{
+	TestingPhase testPhase = CHAR_READ_WRITE;
+	uint8_t testString[SERIAL_PORT3_BUFFERS_LENGTH];
+	uint8_t overloadTestStr[SERIAL_PORT3_BUFFERS_LENGTH + 1];
+	uint32_t i;
+
+	initTestString(testString, SERIAL_PORT3_BUFFERS_LENGTH);
+	initOverloadTestStr(overloadTestStr, SERIAL_PORT3_BUFFERS_LENGTH + 1);
+
+	while (1)
+	{
+		while (testPhase == CHAR_READ_WRITE)
+		{
+			for (i = 0; i < 3; i++)
+			{
+				currentStatus = portInstance->putChar(testString[i],
+						portMAX_DELAY);
+				Delay(100 / portTICK_RATE_MS);
+				currentStatus = portInstance->getChar(&character,
+						portMAX_DELAY);
+			}
+		}
+
+		while (testPhase == STRING_READ_WRITE)
+		{
+
+		}
+
+		while (testPhase == STRING_OVERLOAD)
+		{
+
+		}
+	}
+}
+
+void initTestString(uint8_t* testString, uint32_t stringLength)
+{
+	uint8_t character = 1;
+	uint32_t i;
+
+	for (i = 0; i < stringLength - 1; i++)
+	{
+		testString[i] = character;
+		character++;
+	}
+
+	testString[i] = '\0';
+}
+
+void initOverloadTestStr(uint8_t* ovldTestStr, uint32_t stringLength)
+{
+	uint8_t character = 1;
+	uint32_t i;
+
+	for (i = 0; i < stringLength - 1; i++)
+	{
+		ovldTestStr[i] = character;
+		character++;
+	}
+
+	ovldTestStr[i] = '\0';
+}
--- Source/Libraries/STM32F4xx_FreeRTOS_PeriphDriver/test/AsyncSerialPort3TestTask.h
+++ Source/Libraries/STM32F4xx_FreeRTOS_PeriphDriver/test/AsyncSerialPort3TestTask.h
@@ -1,10 +1,10 @@
 /**
- * \file AsyncSerialPort1TestTask.h
- * \class AsyncSerialPort1TestTask
- * \brief This managed task is used to test the asynchronous serial port 1.
- * 		  Modify and use this task to test the uart 1.
+ * \file AsyncSerialPort3TestTask.h
+ * \class AsyncSerialPort3TestTask
+ * \brief This managed task is used to test the asynchronous serial port 3.
+ * 		  Modify and use this task to test the uart 3.
  *
- * \date Created on: Sep 13, 2013
+ * \date Created on: Dec 7, 2013
  * \date Last change on: &DATE&
  *
  * \author Created by: julien 
@@ -13,18 +13,18 @@
  * \version Commit Id: &REVISION&
  */
 
-#ifndef ASYNCSERIALPORT1TESTTASK_H_
-#define ASYNCSERIALPORT1TESTTASK_H_
+#ifndef ASYNCSERIALPORT3TESTTASK_H_
+#define ASYNCSERIALPORT3TESTTASK_H_
 
 #include "AManagedTask.h"
-#include "AsyncSerialPort1.h"
+#include "AsyncSerialPort3.h"
 #include "CQueue.h"
 #include "SerialType.h"
 
-class AsyncSerialPort1TestTask: public AManagedTask
+class AsyncSerialPort3TestTask: public AManagedTask
 {
 private:
-	AsyncSerialPort1* portInstance;
+	AsyncSerialPort3* portInstance;
 	SerialStatus currentStatus;
 	Parity testParity;
 	StopBits testStopBit;
@@ -35,20 +35,20 @@ private:
 	InterruptSetting testInterSetting;
 
 	int8_t character;
-	int8_t string[SERIAL_PORT1_BUFFERS_LENGTH];
+	int8_t string[SERIAL_PORT3_BUFFERS_LENGTH];
 
 public:
 	/*
 	 * \brief Default Constructor.
 	 */
-	AsyncSerialPort1TestTask(void);
-	AsyncSerialPort1TestTask(const AsyncSerialPort1TestTask&);
-	AsyncSerialPort1TestTask& operator=(const AsyncSerialPort1TestTask&);
+	AsyncSerialPort3TestTask(void);
+	AsyncSerialPort3TestTask(const AsyncSerialPort3TestTask&);
+	AsyncSerialPort3TestTask& operator=(const AsyncSerialPort3TestTask&);
 
 	/*
 	 * \brief Default Destructor.
 	 */
-	virtual ~AsyncSerialPort1TestTask(void);
+	virtual ~AsyncSerialPort3TestTask(void);
 
 	/*
 	 * \brief The hardware initialization of the test task. The input and
@@ -78,4 +78,4 @@ public:
 	void Run(void);
 };
 
-#endif /* ASYNCSERIALPORT1TESTTASK_H_ */
+#endif /* ASYNCSERIALPORT3TESTTASK_H_ */
